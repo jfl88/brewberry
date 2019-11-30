28622499509ab433523767c8e67cce58f39686d5
jfl88 <butters1@gmail.com>
2019-11-30 15:05:04 -0800

    update chart colour method, add version to footer

--- webapp/public/js/brewtest.js
+++ webapp/public/js/brewtest.js
@@ -1,15 +1,15 @@
 (function (){
     'use strict';
 
-    window.chartColors = {
-        red: 'rgb(255, 99, 132)',
-        orange: 'rgb(255, 159, 64)',
-        yellow: 'rgb(255, 205, 86)',
-        green: 'rgb(75, 192, 192)',
-        blue: 'rgb(54, 162, 235)',
-        purple: 'rgb(153, 102, 255)',
-        grey: 'rgb(201, 203, 207)'
-    };
+    window.chartColors = [
+        'rgb(54, 162, 235)',
+        'rgb(255, 99, 132)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 159, 64)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+    ];
 
     angular
         .module('brewtest', ['nvd3'])
@@ -61,8 +61,8 @@
                             label: controller.name,
                             fill: false,
                             pointRadius: 0,
-                            backgroundColor: window.chartColors.blue,
-                            borderColor: window.chartColors.blue,
+                            backgroundColor: window.chartColors[idx % window.chartColors.length],
+                            borderColor: window.chartColors[idx % window.chartColors.length],
                             data: []
                         }
 
