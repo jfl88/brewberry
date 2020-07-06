b5ee9a52f1042d7d87dc39f28c7f433d94f6be06
Jason Lyons <jlyons@vanrx.com>
2020-07-06 12:20:11 -0700

    fix require for moment timezone

--- webapp/app.js
+++ webapp/app.js
@@ -12,7 +12,7 @@ var control = require('./routes/control');
 const logger = require('../logger');
 
 var app = express();
-app.locals.moment = require('moment');
+app.locals.moment = require('moment-timezone');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
