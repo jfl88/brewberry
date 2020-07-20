963eb066a4a5df84cea1cc3a57ef7aa623eb2dbd
Jason Lyons <jlyons@vanrx.com>
2020-07-19 18:12:44 -0700

    start working on edit/new ctrlr form

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,18 +5,18 @@ const Output = require('../outputs/output');
 const logger = require('../logger');
 
 class Controller {
-    static newController(controller){
-        logger.debug('controller.js: \n%o', controller);
-        switch (controller.model){
-            case 'ReadOnly':
-              return new ReadOnly(controller.id, controller.name, Sensor.newSensor(controller.sensor), controller.output, controller.updateRate, controller.param);
-            case 'Hysteresis':
-              return new Hysteresis(controller.id, controller.name, Sensor.newSensor(controller.sensor), Output.newOutput(controller.output), controller.updateRate, controller.param);
-            default:
-              logger.error('controller.js: controller model does not exist');
-              return undefined;
-          }
+  static newController(controller){
+    logger.debug('controller.js: \n%o', controller);
+    switch (controller.model){
+      case 'ReadOnly':
+        return new ReadOnly(controller.id, controller.name, Sensor.newSensor(controller.sensor), controller.output, controller.updateRate, controller.param);
+      case 'Hysteresis':
+        return new Hysteresis(controller.id, controller.name, Sensor.newSensor(controller.sensor), Output.newOutput(controller.output), controller.updateRate, controller.param);
+      default:
+        logger.error('controller.js: controller model' & controller.model & 'does not exist');
+        return undefined;
     }
+  }
 }
 
 module.exports = Controller;
